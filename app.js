812933fb8cfba8fdcc5e3976ed8b8150b8cf779a
raphhawk <bing619.at@gmail.com>
2022-11-19 21:57:29 +0530

    pug_complete

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const path        =  require("path");
 const express     =  require("express");
 const bodyParser  =  require("body-parser");
+const expressHbs  =  require("express-handlebars");
 const adminData   =  require("./routes/admin");
 const shopRoutes  =  require("./routes/shop");
 
+
+
 const app = express();
 
-app.set("view engine", "pug");
+app.engine("hbs", expressHbs);
+app.set("view engine", "hbs");
 app.set("views", "views");
+//app.set("view engine", "pug");
+//app.set("views", "views");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next)=>{
-  res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
+  //res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
+  res.status(404).render("not-found", {pageTitle: "Not Found"});
 });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get("/", (req, res, next)=>{
   console.log("executiing middleware at /");
   //res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminData.products;
-  res.render("shop", {prods: products, docTitle: "Shop"});
+  res.render("shop", {prods: products, pageTitle: "Shop"});
 });
 
 
