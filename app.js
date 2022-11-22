983ac707358d631be4ea1ed8672107294a15569d
raphhawk <bing619.at@gmail.com>
2022-11-22 12:32:38 +0530

    error fix

--- app.js
+++ app.js
@@ -3,19 +3,11 @@ const express     =  require("express");
 const bodyParser  =  require("body-parser");
 const adminData   =  require("./routes/admin");
 const shopRoutes  =  require("./routes/shop");
-const expressHbs  =  require("express-handlebars");
 
 const app = express();
 
-app.engine("hbs", expressHbs({
-  defaultLayout: "main-layout",
-  extname: ".hbs",
-  layoutsDir: path.join(__dirname, "/views/layouts")
-}));
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
-//app.set("view engine", "pug");
-//app.set("views", "views");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("/admin", adminData.routes);
--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,7 @@ router.get("/", (req, res, next)=>{
   const products = adminData.products;
   res.render("shop", {
     prods: products, 
+    path: "/",
     pageTitle: "Shop", 
     hasProducts: products.length > 0,
     activeShop: true,
